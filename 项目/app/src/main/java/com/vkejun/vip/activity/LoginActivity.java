b9697da8fe5a15954ce2892af001757ebb9d0ba5
LINYUANZHUO <1135969606@qq.com>
2020-01-23 15:52:23 +0800

    添加登录动画

--- 项目/app/src/main/java/com/vkejun/vip/activity/LoginActivity.java
+++ 项目/app/src/main/java/com/vkejun/vip/activity/LoginActivity.java
@@ -5,12 +5,32 @@ import android.os.Bundle;
 import com.vkejun.vip.R;
 import android.view.View;
 import android.content.Intent;
+import com.vkejun.vip.utils.OwlView;
+import android.widget.EditText;
 
-public class LoginActivity extends AppCompatActivity{
+public class LoginActivity extends AppCompatActivity {
+
+    private OwlView mOwlView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        
+        mOwlView=(OwlView)findViewById(R.id.aaa);
+        EditText eee=(EditText)findViewById(R.id.mi);
+        eee.setOnFocusChangeListener(new android.view.View.OnFocusChangeListener() {
+            //登录动画
+                @Override
+                public void onFocusChange(View v, boolean hasFocus) {
+                    if (hasFocus) {
+                        mOwlView.open();
+                        // 获得焦点
+                    } else {
+                        mOwlView.close();
+                        // 失去焦点
+                    }
+                }
+            });
     }
     
     public void zhaohuimima(View view) {
